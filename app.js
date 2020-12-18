b8f55e1a4ac17db4cf4d24d9216eb1349d063f29
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-18 15:57:28 +0100

    modif

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ let buttonA = document.getElementById("alert");
 let buttonS = document.getElementById("somme");
 
 buttonA.addEventListener("click", () => {alert("Hello world");});
+buttonA.addEventListener("keypress", (event) => {
+    if(event.key ==="enter") {
+        alert("Hello world");
+    }
+});
+
 buttonS.addEventListener("click", () => {
     let div = document.createElement("div");
     document.body.append(div);
